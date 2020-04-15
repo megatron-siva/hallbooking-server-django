fb2759b3b99d803b2279ae16b34ec8aae94bb16d
Megatron Siva <sivappriyansandeep@gmail.com>
2020-04-15 21:06:21 +0530

    jj

--- booking/settings.py
+++ booking/settings.py
@@ -129,6 +129,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'login/static')]
 
 
-#import django_heroku
-#django_heroku.settings(locals())
+import django_heroku
+django_heroku.settings(locals())
 
