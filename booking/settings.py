302d5a4a2413626a738d0254132aa034db2f30c6
Megatron Siva <sivappriyansandeep@gmail.com>
2020-04-15 20:54:16 +0530

    jj

--- booking/settings.py
+++ booking/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'login/static')]
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 
 
 #import django_heroku
