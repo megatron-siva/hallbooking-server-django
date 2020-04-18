a22391d7669f45d7c5ab9ce864f7666ac484f5db
Megatron Siva <sivappriyansandeep@gmail.com>
2020-04-18 20:19:19 +0530

    model name add

--- login/models.py
+++ login/models.py
@@ -60,7 +60,7 @@ class Request(models.Model):
     hall_incharge_id = models.CharField(max_length=50)
     hall_name = models.CharField(max_length=50)
     def __str__(self):
-        return (self.request_id)+' - '+self.userid+' - '+self.hall_id
+        return str(self.request_id)+' - '+self.userid+' - '+self.hall_id
 
 class Booked(models.Model):
     booking_id=models.AutoField(primary_key=True,default=None)
