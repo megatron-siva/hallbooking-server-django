cfbc5c7290453a87e500dd7905659cdcf107317b
Megatron $iva <sivappriyansandeep@gmail.com>
2021-01-22 08:16:16 +0530

    removing STATICFILES_DIRS

--- booking/settings.py
+++ booking/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'login/static')]
+#STATICFILES_DIRS=[os.path.join(BASE_DIR,'login/static')]
 import django_heroku
 
 django_heroku.settings(locals())
