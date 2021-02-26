5ffcbbe6f16a1263daaab492ba1bee8b83b36f17
Megatron Siva <sivappriyansandeep@gmail.com>
2021-02-26 15:43:32 +0530

    generator added

--- login/admin.py
+++ login/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Login,Hall,Booked,Request,Hod_Dean,Principal,Staff
+from .models import Login, Hall, Booked, Request, Hod_Dean, Principal, Staff, Generator_Incharge
 
 # Register your models here.
 admin.site.register(Login)
@@ -9,3 +9,4 @@ admin.site.register(Request)
 admin.site.register(Hod_Dean)
 admin.site.register(Staff)
 admin.site.register(Principal)
+admin.site.register(Generator_Incharge)
